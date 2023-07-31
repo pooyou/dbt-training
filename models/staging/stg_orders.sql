13054e03787e64c04757702742344b2449106a11
pooyou@gmail.com <pooyou@gmail.com>
2023-07-31 07:46:59 +0000

    developing stage_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,23 @@
+select
+-- from raw_ orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercost as orderprofit,
+o.ordercost,
+o.ordersellingprice,
+-- From raw customer
+c.customername,
+c.segment,
+c.country,
+-- From product
+p.category,
+p.productname,
+p.subcategory
+
+from {{ ref("raw_orders") }} as o
+left join {{ ref('raw_customer') }} as c
+ON o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+ON o.productid = p.productid
