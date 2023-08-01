fad33b8cbb5cdae6beac4b6f186a1e085fd80018
pooyou@gmail.com <pooyou@gmail.com>
2023-08-01 11:31:12 +0000

    added delivery_team as seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select
 -- from raw_ orders
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders,
 o.orderid,
 o.orderdate,
 o.shipdate,
@@ -17,11 +18,15 @@ p.productid,
 p.category,
 p.productname,
 p.subcategory,
-{{ markup('o.ordersellingprice', 'o.ordercost') }} as markup
-
+{{ markup('o.ordersellingprice', 'o.ordercost') }} as markup,
+d.delivery_team
 
 from {{ ref("raw_orders") }} as o
 left join {{ ref('raw_customer') }} as c
 ON o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 ON o.productid = p.productid
+LEFT JOIN {{ ref('delivery_team') }} d
+ON o.shipmode = d.shipmode
+
+{{limit_data_in_dev('orderdate')}}
